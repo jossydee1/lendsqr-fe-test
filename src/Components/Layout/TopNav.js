7b3788beee77fc8cb2d5e51886cd352b0c373d2c
jossydee1 <jossydee1@gmail.com>
2023-01-09 14:18:36 +0100

    only sidebar deployed

--- src/Components/Layout/TopNav.js
+++ src/Components/Layout/TopNav.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import logo from "../../Assets/logo.png";
-import searchIcon from "../../Assets/TopNav/search.png";
-import bell from "../../Assets/TopNav/bell.png";
-import avatar from "../../Assets/TopNav/avatar.png";
-import dropdown from "../../Assets/TopNav/dropdown.png";
+// import searchIcon from "../../Assets/TopNav/search.png";
+// import bell from "../../Assets/TopNav/bell.png";
+// import avatar from "../../Assets/TopNav/avatar.png";
+// import dropdown from "../../Assets/TopNav/dropdown.png";
 import "../../Pages/Dashboard/Dashboard.scss";
 import AppContext from "../../Context/Context";
 
@@ -25,19 +25,19 @@ function TopNav() {
         <div className="search-box">
           <input type="search" placeholder="Search for anything" />
           <div className="search-icon">
-            <img src={searchIcon} alt="icon" className="icon" />
+            {/* <img src={searchIcon} alt="icon" className="icon" /> */}
           </div>
         </div>
         <div className="right">
           {!phoneSize && (
             <>
               <p className="docs">Docs</p>
-              <img src={bell} alt="bell" className="bell" />
+              {/* <img src={bell} alt="bell" className="bell" /> */}
             </>
           )}
-          <img src={avatar} alt="avatar" className="avatar" />
+          {/* <img src={avatar} alt="avatar" className="avatar" /> */}
           <p className="admin">Adedeji</p>
-          <img src={dropdown} alt="icon" className="dropdown" />
+          {/* <img src={dropdown} alt="icon" className="dropdown" /> */}
         </div>
       </div>
     </div>
